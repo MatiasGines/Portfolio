5925f63eedbb45a9cbdf0695d5c14c1f2aa57862
MatiasGines <41976674+MatiasGines@users.noreply.github.com>
2022-03-27 20:14:58 -0300

    modificación
    modificación

--- js/main.js
+++ js/main.js
@@ -3,22 +3,24 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   });
 
 
-window.sr = ScrollReveal();
 
+  window.sr = ScrollReveal();
+  
   sr.reveal('.icono', {
     duration: 3000,
     origin: 'bottom',
-    distance: '-100px',
+    distance: '100px',
   });
-
+  
   sr.reveal('.itemleft', {
     duration: 3000,
     origin: 'left',
-    distance: '-200px',
+    distance: '100px',
   });
-
+  
   sr.reveal('.itemright', {
     duration: 3000,
     origin: 'right',
-    distance: '-200px',
+    distance: '-100px',
   });
+  
